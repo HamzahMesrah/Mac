fe27a1be5e3befee355b65c7d62f1a490888befe
Hamzah <193113920+HamzahMesrah@users.noreply.github.com>
2025-09-29 23:13:12 +0300

    Final

---
+++ components/icons/MagicWandIcon.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+export const MagicWandIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+        <path d="M15 4V2" />
+        <path d="M15 22v-2" />
+        <path d="M12.24 9.76a6 6 0 0 1 8.49-8.49" />
+        <path d="m3 7 3 3" />
+        <path d="M5 6v4" />
+        <path d="M19 6v4" />
+        <path d="M22 15h-2" />
+        <path d="M4 15H2" />
+        <path d="m16.76 12.24-8.49 8.49" />
+    </svg>
+);
